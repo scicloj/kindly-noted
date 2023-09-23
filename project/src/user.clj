93145de2f8f77f2f629bfc8ae12e61236430faf6
daslu <daniel.slutsky@gmail.com>
2023-09-23 23:43:17 +0300

    changed clay quarto options

--- project/src/user.clj
+++ project/src/user.clj
@@ -9,9 +9,9 @@
  :remote-repo {:git-url "https://github.com/scicloj/kindly-noted"
                :branch "main"}
  :quarto {:format {:html {:toc true
-                          :theme :spacelab
-                          :embed-resources true}}
+                          :toc-depth 4
+                          :theme :spacelab}}
           :highlight-style :solarized
           :code-block-background true
-          :embed-resources true
+          :embed-resources false
           :execute {:freeze true}})
