dc0d39c52cd354edf593cfb0165b63d797f3a617
daslu <daniel.slutsky@gmail.com>
2024-01-14 20:20:45 +0200

    book setup

---
+++ notebooks/dev.clj
@@ -0,0 +1,11 @@
+(ns dev
+  (:require [scicloj.clay.v2.api :as clay]))
+
+(clay/make! {:format [:quarto :html]
+             :base-source-path "notebooks"
+             :source-path ["index.clj"
+                           "kindly.clj"
+                           "kindly_advice.clj"]
+             :base-target-path "docs"
+             :book {:title "Kindly Noted"}
+             :clean-up-target-dir true})
