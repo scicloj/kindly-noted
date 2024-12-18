d9a135e84d8311b0403ccf37acec7c0e70f2a50b
daslu <daniel.slutsky@gmail.com>
2024-12-18 10:45:51 +0200

    typos

--- notebooks/kindly_advice.clj
+++ notebooks/kindly_advice.clj
@@ -37,7 +37,7 @@ The `:kind` field is the important one, expressing the bottom line of the infere
 
 The tool's job will usually be to display the `:value` field based on the `:kind` field.
 
-In the following exmple, we are asking for advice for given form (annotated by Kindly in this example).
+In the following example, we are asking for advice for given form (annotated by Kindly in this example).
 Kindly-advice evaluates the form and adds the resulting value to complete the context. This completion will only take place if the value is missing. It is recommended that tools will take care of evaluation themselves and pass the complete context to Kindly-advice. Doing so allows the tool to handle Exceptions better, among other things.
 Kindly-advice checks both the form and value for metadata. The metadata might not be present on the value.
 ")
@@ -53,7 +53,7 @@ Kindly-advice checks both the form and value for metadata. The metadata might no
     kindly-advice/advise
     kind/pprint)
 
-(md "In some situations, the kind inferred by predicates. Kindly-advice has a list of default predicates, which can be extended by the user. In the following eexample, it recognizes a dataset created by Tablecloth.")
+(md "In some situations, the kind inferred by predicates. Kindly-advice has a list of default predicates, which can be extended by the user. In the following example, it recognizes a dataset created by Tablecloth.")
 
 (require '[tablecloth.api :as tc])
 (-> {:value (tc/dataset {:x (range 4)})}
