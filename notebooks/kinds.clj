3caaa73c5305ed9f6c8ed90d1416472906bdd5a9
daslu <daniel.slutsky@gmail.com>
2025-01-13 23:39:32 +0200

    updated kind/cytoscape example

--- notebooks/kinds.clj
+++ notebooks/kinds.clj
@@ -256,32 +256,44 @@ vega-lite-plot
                            :y {:scale :yscale
                                :field :amount}
                            :y2 {:scale :yscale
-                                :value 0}}}}})
+                                :value 0}}
+                   :update {:fill
+                            {:value :steelblue}}
+                   :hover {:fill
+                           {:value :red}}}}})
 
 
 ;; ## Cytoscape
 
+(def cytoscape-example
+  {:elements {:nodes [{:data {:id "a" :parent "b"} :position {:x 215 :y 85}}
+                      {:data {:id "b"}}
+                      {:data {:id "c" :parent "b"} :position {:x 300 :y 85}}
+                      {:data {:id "d"} :position {:x 215 :y 175}}
+                      {:data {:id "e"}}
+                      {:data {:id "f" :parent "e"} :position {:x 300 :y 175}}]
+              :edges [{:data {:id "ad" :source "a" :target "d"}}
+                      {:data {:id "eb" :source "e" :target "b"}}]}
+   :style [{:selector "node"
+            :css {:content "data(id)"
+                  :text-valign "center"
+                  :text-halign "center"}}
+           {:selector "parent"
+            :css {:text-valign "top"
+                  :text-halign "center"}}
+           {:selector "edge"
+            :css {:curve-style "bezier"
+                  :target-arrow-shape "triangle"}}]
+   :layout {:name "preset"
+            :padding 5}})
+
 (kind/cytoscape
- {:elements {:nodes [{:data {:id "a" :parent "b"} :position {:x 215 :y 85}}
-                     {:data {:id "b"}}
-                     {:data {:id "c" :parent "b"} :position {:x 300 :y 85}}
-                     {:data {:id "d"} :position {:x 215 :y 175}}
-                     {:data {:id "e"}}
-                     {:data {:id "f" :parent "e"} :position {:x 300 :y 175}}]
-             :edges [{:data {:id "ad" :source "a" :target "d"}}
-                     {:data {:id "eb" :source "e" :target "b"}}]}
-  :style [{:selector "node"
-           :css {:content "data(id)"
-                 :text-valign "center"
-                 :text-halign "center"}}
-          {:selector "parent"
-           :css {:text-valign "top"
-                 :text-halign "center"}}
-          {:selector "edge"
-           :css {:curve-style "bezier"
-                 :target-arrow-shape "triangle"}}]
-  :layout {:name "preset"
-           :padding 5}})
+ cytoscape-example)
+
+(-> cytoscape-example
+    (kind/cytoscape {:style
+                     {:width "100px"
+                      :height "100px"}}))
 
 ;; ## ECharts
 
