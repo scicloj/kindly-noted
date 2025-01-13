d434f5f5d78cc5043107deff5b664dc48be00368
daslu <daniel.slutsky@gmail.com>
2025-01-13 23:35:38 +0200

    added vega example

--- notebooks/kinds.clj
+++ notebooks/kinds.clj
@@ -209,6 +209,50 @@ hello-hiccup
 
 vega-lite-plot
 
+;; ## Vega
+
+;; Inspired by [Let's Make A Bar Chart Tutorial](https://vega.github.io/vega/tutorials/bar-chart/) of the [Vega](https://vega.github.io/vega/) docs.
+
+(kind/vega
+ {:$schema "https://vega.github.io/schema/vega/v5.json"
+  :width 400
+  :height 200
+  :padding 5
+  :data {:name "table"
+         :values [{:category :A :amount 28}
+                  {:category :B :amount 55}
+                  {:category :C :amount 43}
+                  {:category :D :amount 91}
+                  {:category :E :amount 81}
+                  {:category :F :amount 53}
+                  {:category :G :amount 19}
+                  {:category :H :amount 87}]}
+  :scales [{:name :xscale
+            :type :band
+            :domain {:data :table
+                     :field :category}
+            :range :width
+            :padding 0.05
+            :round true}
+           {:name :yscale
+            :domain {:data :table
+                     :field :amount}
+            :nice true
+            :range :height}]
+  :axes [{:orient :bottom :scale :xscale}
+         {:orient :left :scale :yscale}]
+  :marks {:type :rect
+          :from {:data :table}
+          :encode {:enter {:x {:scale :xscale
+                               :field :category}
+                           :width {:scale :xscale
+                                   :band 1}
+                           :y {:scale :yscale
+                               :field :amount}
+                           :y2 {:scale :yscale
+                                :value 0}}}}})
+
+
 ;; ## Cytoscape
 
 (kind/cytoscape
