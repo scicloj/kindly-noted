f18f52c229ba8eac731fd3062347e2ef56ada2cd
daslu <daniel.slutsky@gmail.com>
2025-01-26 17:38:45 +0200

    updated clerk in compatibility table

--- notebooks/kind_compatibility.clj
+++ notebooks/kind_compatibility.clj
@@ -10,42 +10,41 @@
             ))
 ^:kindly/hide-code
 (def kind-status
-  [
-   {:clojupyter :n, :description :kind/edn, :clay :c, :clerk :u, :kind :kind/edn}
-   {:clojupyter :r, :description :kind/code, :clay :c, :clerk :u, :kind :kind/code}
-   {:clojupyter :n, :description :kind/vega, :clay :c, :clerk :u, :kind :kind/vega}
-   {:clojupyter :n, :description :kind/smile-model, :clay :c, :clerk :u, :kind :kind/smile-model}
-   {:clojupyter :c, :description :kind/image, :clay :c, :clerk :u, :kind :kind/image}
-   {:clojupyter :c, :description :kind/plotly, :clay :c, :clerk :u, :kind :kind/plotly}
+  [{:clojupyter :n, :description :kind/edn, :clay :c, :clerk :n, :kind :kind/edn}
+   {:clojupyter :r, :description :kind/code, :clay :c, :clerk :n, :kind :kind/code}
+   {:clojupyter :n, :description :kind/vega, :clay :c, :clerk :c, :kind :kind/vega}
+   {:clojupyter :n, :description :kind/smile-model, :clay :c, :clerk :n, :kind :kind/smile-model}
+   {:clojupyter :c, :description :kind/image, :clay :c, :clerk :r, :kind :kind/image}
+   {:clojupyter :c, :description :kind/plotly, :clay :c, :clerk :c, :kind :kind/plotly}
    {:clojupyter :c, :description :kind/echarts, :clay :c, :clerk :u, :kind :kind/echarts}
-   {:clojupyter :r, :description :kind/map, :clay :c, :clerk :u, :kind :kind/map}
-   {:clojupyter :n, :description :kind/portal, :clay :c, :clerk :u, :kind :kind/portal}
-   {:clojupyter :r, :description :kind/test, :clay :c, :clerk :u, :kind :kind/test}
-   {:clojupyter :c, :description :kind/dataset, :clay :c, :clerk :u, :kind :kind/dataset}
-   {:clojupyter :c, :description :kind/vega-lite, :clay :c, :clerk :u, :kind :kind/vega-lite}
-   {:clojupyter :c, :description :kind/html, :clay :c, :clerk :u, :kind :kind/html}
-   {:clojupyter :c, :description :kind/cytoscape, :clay :c, :clerk :u, :kind :kind/cytoscape}
-   {:clojupyter :r, :description :kind/set, :clay :c, :clerk :u, :kind :kind/set}
-   {:clojupyter :n, :description :kind/reagent, :clay :c, :clerk :u, :kind :kind/reagent}
-   {:clojupyter :n, :description :kind/var, :clay :c, :clerk :u, :kind :kind/var}
-   {:clojupyter :c, :description :kind/hidden, :clay :c, :clerk :u, :kind :kind/hidden}
-   {:clojupyter :c, :description :kind/hiccup, :clay :c, :clerk :u, :kind :kind/hiccup}
-   {:clojupyter :c, :description :kind/md, :clay :c, :clerk :u, :kind :kind/md}
-   {:clojupyter :r, :description :kind/tex, :clay :c, :clerk :u, :kind :kind/tex}
-   {:clojupyter :r, :description :kind/seq, :clay :c, :clerk :u, :kind :kind/seq}
-   {:clojupyter :n, :description :kind/htmlwidgets-plotly, :clay :c, :clerk :u, :kind :kind/htmlwidgets-plotly}
-   {:clojupyter :r, :description :kind/video, :clay :c, :clerk :u, :kind :kind/video}
-   {:clojupyter :n, :description :kind/observable, :clay :c, :clerk :u, :kind :kind/observable}
-   {:clojupyter :n, :description :kind/emmy-viewers, :clay :c, :clerk :u, :kind :kind/emmy-viewers}
-   {:clojupyter :r, :description :kind/pprint, :clay :c, :clerk :u, :kind :kind/pprint}
-   {:clojupyter :c, :description :kind/highcharts, :clay :c, :clerk :u, :kind :kind/highcharts}
-   {:clojupyter :r, :description :kind/table, :clay :c, :clerk :u, :kind :kind/table}
-   {:clojupyter :e, :description :kind/fn, :clay :c, :clerk :u, :kind :kind/fn}
-   {:clojupyter :r, :description :kind/vector, :clay :c, :clerk :u, :kind :kind/vector}
+   {:clojupyter :r, :description :kind/map, :clay :c, :clerk :c, :kind :kind/map}
+   {:clojupyter :n, :description :kind/portal, :clay :c, :clerk :n, :kind :kind/portal}
+   {:clojupyter :r, :description :kind/test, :clay :c, :clerk :n, :kind :kind/test}
+   {:clojupyter :c, :description :kind/dataset, :clay :c, :clerk :c, :kind :kind/dataset}
+   {:clojupyter :c, :description :kind/vega-lite, :clay :c, :clerk :c, :kind :kind/vega-lite}
+   {:clojupyter :c, :description :kind/html, :clay :c, :clerk :n, :kind :kind/html}
+   {:clojupyter :c, :description :kind/cytoscape, :clay :c, :clerk :c, :kind :kind/cytoscape}
+   {:clojupyter :r, :description :kind/set, :clay :c, :clerk :c, :kind :kind/set}
+   {:clojupyter :n, :description :kind/reagent, :clay :c, :clerk :n, :kind :kind/reagent}
+   {:clojupyter :n, :description :kind/var, :clay :c, :clerk :n, :kind :kind/var}
+   {:clojupyter :c, :description :kind/hidden, :clay :c, :clerk :n, :kind :kind/hidden}
+   {:clojupyter :c, :description :kind/hiccup, :clay :c, :clerk :c, :kind :kind/hiccup}
+   {:clojupyter :c, :description :kind/md, :clay :c, :clerk :c, :kind :kind/md}
+   {:clojupyter :r, :description :kind/tex, :clay :c, :clerk :n, :kind :kind/tex}
+   {:clojupyter :r, :description :kind/seq, :clay :c, :clerk :c, :kind :kind/seq}
+   {:clojupyter :n, :description :kind/htmlwidgets-plotly, :clay :c, :clerk :n, :kind :kind/htmlwidgets-plotly}
+   {:clojupyter :r, :description :kind/video, :clay :c, :clerk :n, :kind :kind/video}
+   {:clojupyter :n, :description :kind/observable, :clay :c, :clerk :n, :kind :kind/observable}
+   {:clojupyter :n, :description :kind/emmy-viewers, :clay :c, :clerk :n, :kind :kind/emmy-viewers}
+   {:clojupyter :r, :description :kind/pprint, :clay :c, :clerk :n, :kind :kind/pprint}
+   {:clojupyter :c, :description :kind/highcharts, :clay :c, :clerk :n, :kind :kind/highcharts}
+   {:clojupyter :r, :description :kind/table, :clay :c, :clerk :r, :kind :kind/table}
+   {:clojupyter :e, :description :kind/fn, :clay :c, :clerk :n, :kind :kind/fn}
+   {:clojupyter :r, :description :kind/vector, :clay :c, :clerk :c, :kind :kind/vector}
    {:clojupyter :n, :description :kind/htmlwidgets-ggplotly, :clay :c, :clerk :u, :kind :kind/htmlwidgets-ggplotly}
-   {:clojupyter :n, :description :kind/fragment, :clay :c, :clerk :u, :kind :kind/fragment}
-   {:clojupyter :n, :description :kind/scittle, :clay :c, :clerk :u, :kind :kind/scittle}
-   {:clojupyter :r, :description :kind/test-last, :clay :c, :clerk :u, :kind :kind/test-last}
+   {:clojupyter :n, :description :kind/fragment, :clay :c, :clerk :n, :kind :kind/fragment}
+   {:clojupyter :n, :description :kind/scittle, :clay :c, :clerk :n, :kind :kind/scittle}
+   {:clojupyter :r, :description :kind/test-last, :clay :c, :clerk :n, :kind :kind/test-last}
 
 
    ])
@@ -60,12 +59,12 @@
                   :n (kind/hiccup [:div {:style "background-color:red"} (-> v name str/upper-case)])
                   :e (kind/hiccup [:div {:style "background-color:red"} (-> v name str/upper-case)])
                   :r (kind/hiccup [:div {:style "background-color:yellow"} (-> v name str/upper-case)])
-                  ;(-> v name str/upper-case)
+                                        ;(-> v name str/upper-case)
 
                   )
                 
                 )
-    
+          
           ))
 
 ^:kindly/hide-code
